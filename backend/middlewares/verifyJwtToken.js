635232fbf61823da97c5f88d77816fa34f2476ac
TristanSch1 <tristan.sch57@gmail.com>
2022-04-28 22:25:00 +0200

    Install json web token

---
+++ backend/middlewares/verifyJwtToken.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+require("dotenv/config");
+/* Vérification du token */
+const checkTokenMiddleware = (req, res, next) => {
+    // Récupération du token
+    const token = req.headers.authorization.split(" ");
+
+    // Présence d'un token
+    if (!token[1]) {
+        return res.status(401).json({ message: "Error. Need a token" });
+    }
+    // Véracité du token
+    jwt.verify(token[1], process.env.SECRET, (err) => {
+        if (err) {
+            res.status(401).json({ message: "Error. Bad token" });
+        } else {
+            return next();
+        }
+    });
+};
+
+module.exports = checkTokenMiddleware;
